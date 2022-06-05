36c803f25d5cd198088f931bda67e8e013145229
Your Name <you@example.com>
2022-06-05 22:53:33 +0100

    First

---
+++ backend-client/src/enums/entities.enum.js
@@ -0,0 +1,44 @@
+export const pressTypes = { rapport: "Rapport", article: "Article", brochure: "Brochure", communique: "Communique" }
+export const unitTypes = {
+  ticCommun: "TIC Tronc Commun",
+  geCommun: "Genie Electromécanique Tronc Commun",
+  ticDeuxieme: "TIC Deuxième Cycle",
+  tcDeuxieme: "Télécom Deuxième Cycle",
+  geDeuxieme: "Genie Electromécanique Deuxième Cycle",
+  genieCivil: "Genie Civil",
+}
+export const partnershipTypes = { technological: "Technologique", academic: "Académique", industrial: "industriel" }
+export const productionTypes = {
+  ARTICLE_SC: "Article scientifique",
+  CONFERENCE: "Conférence",
+  CONTRIBUTION_L: "Contribution de livre",
+  PRODUCTION_TECH: "Production technique",
+}
+export const clubTypes = { sports: "Sports", other: "Autre" }
+export const sportTypes = { individual: "Individuel", team: "Équipe" }
+export const timeTableTitles = {
+  calendrierAnnuel: "Calendrier Annuel",
+  calendrierAnnuelCinquieme: "Calendrier Annuel 5ème année",
+  calendrierGeneral: "Calendrier Général",
+  calendrierCinquieme: "Calendrier 5ème année",
+  calendrierExamens: "Calendrier Examens",
+  calendrierRattrapage: "Calendrier Examens Rattrapages",
+  calendrierExamensCinquieme: "Calendrier Examens 5ème année",
+  calendrierRattrapageCinquieme: "Calendrier Examens Rattrapages 5ème année",
+  reglement: "Réglement durant les examens",
+  calendrierDadmission: "Calendrier d'admission",
+  grilleTarifaireInternationaux: "Grille tarifaire pour internationaux",
+  grilleTarifaireTunisiens: "Grille tarifaire pour Tunisiens",
+  contratFoyerBloc1: "Contrat pour le foyer bloc1",
+  contratFoyerBloc2: "Contrat pour le foyer bloc2",
+  reglementDuFoyer: "Règlement intérieur du foyer",
+  reglementDeLecole: "Règlement intérieur de l'école",
+}
+export const eventTypes = {
+  actualite: "Actualité",
+  evenement: "Événement",
+}
+export const tefTefaqTypes = {
+  tef: "tef",
+  tefaq: "tefaq",
+}
